5869018d06a6dd18d0db5baabc3d3d57f3439fc8
helenou <ln.lejeune@gmail.com>
2019-11-27 21:36:08 +0100

    first boot

---
+++ src/main/java/fr/helenou/springbootForumJpa/SpringForumApplication.java
@@ -0,0 +1,13 @@
+package fr.helenou.springbootForumJpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringForumApplication.class, args);
+	}
+
+}
